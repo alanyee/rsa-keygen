e4d882282125077841643714cc74febc1e5e88a8
Alan Yee <alanyee@users.noreply.github.com>
2021-10-03 10:30:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,27 @@
 
 from setuptools import find_packages, setup
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
 setup(
-    name='rsa-keygen',
+    name="rsa-keygen",
     entry_points={
-      'console_scripts': [
-          'rsa-keygen = rsactf.rsa:main',
+      "console_scripts": [
+          "rsa-keygen = rsactf.rsa:main",
       ],
     },
-    version='0.0.1',
-    description='Generate large textbook integer-type RSA schema',
+    version="0.0.1",
+    description="Generate large textbook integer-type RSA schema",
     long_description=readme,
-    author='Alan Yee',
-    author_email='alanyee@users.noreply.github.com',
-    url='https://github.com/alanyee/rsa-keygen',
+    author="Alan Yee",
+    author_email="alanyee@users.noreply.github.com",
+    url="https://github.com/alanyee/rsa-keygen",
     packages=["rsactf"],
     include_package_data=True,
-    python_requires='>=3.8',
+    python_requires=">=3.8",
+    install_requires=["pycryptodomex"],
     extras_require={
-        'testing': ["pylint", "pytest", "mypy"]
+        "testing": ["pylint", "pytest", "mypy"]
     }
 )
