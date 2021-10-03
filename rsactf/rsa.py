4380269f3f0e7e1a3e755f64041ad118991040c7
Lyndon Fan <lyndon0808@gmail.com>
2021-10-03 18:18:02 +0100

    Generate primes (#15)
    * gen big primes (+ supply length)
    
    * mention priority
    
    * update readme
    
    * remove debug print
    
    * primes > prime_length(s)
    
    * undo README change
    
    * add package dependency
    
    * attempt to fix lint
    
    * attempt to fix lint
    
    * minor update on main
    
    * minor edits
    
    * lint with black

--- rsactf/rsa.py
+++ rsactf/rsa.py
@@ -1,18 +1,54 @@
 import argparse
 import math
 import sys
+from Cryptodome.Util import number
+
+N_LENGTH = 2048
 
 
 def _parse_args():
-    parser = argparse.ArgumentParser(description='CTF RSA Generator')
-    parser.add_argument("-m", "--message", type=str,
-                        help="Message to be encrypted and decrypted")
-    parser.add_argument("-p", "--primes", nargs=2, type=int, metavar="prime",
-                        help="Load from two big prime numbers")
-    parser.add_argument("-e", "--exponent", type=int, metavar="exponent", default=65537,
-                        help="Load from exponent")
+    parser = argparse.ArgumentParser(description="CTF RSA Generator")
+    parser.add_argument(
+        "-m", "--message", type=str, help="Message to be encrypted and decrypted"
+    )
+    parser.add_argument(
+        "-l",
+        "--prime-length",
+        nargs=1,
+        type=int,
+        metavar="prime_length",
+        default=N_LENGTH,
+        help="Generate both prime numbers with the given length (in bytes)",
+    )
+    parser.add_argument(
+        "-ls",
+        "--prime-lengths",
+        nargs=2,
+        type=int,
+        metavar="prime_lengths",
+        help="Generate prime numbers from the given length (in bytes)."
+        + "Takes precedence over prime_length.",
+    )
+    parser.add_argument(
+        "-p",
+        "--primes",
+        nargs=2,
+        type=int,
+        metavar="prime",
+        default=[0, 0],
+        help="Load from two big prime numbers. Takes precedence over prime_length and prime_lengths.",
+    )
+    parser.add_argument(
+        "-e",
+        "--exponent",
+        type=int,
+        metavar="exponent",
+        default=65537,
+        help="Load from exponent",
+    )
 
     return parser, parser.parse_args()
+
 
 def main():
     parser, args = _parse_args()
@@ -25,11 +61,24 @@ def main():
         print("m:")
         print(m)
 
+    for i in range(2):
+        if args.primes[i] == 0:
+            p_len = (
+                args.prime_lengths[i] if args.prime_lengths else args.prime_length[0]
+            )
+            print(
+                f"Generating {'1st' if i==0 else '2nd'} prime of length {p_len}",
+                end=" ... ",
+            )
+            sys.stdout.flush()  # force sys to print
+            args.primes[i] = number.getPrime(p_len)
+            print("Done.")
+
     if args.primes:
         n = math.prod(args.primes)
         print("n:")
         print(n)
-        phi = (args.primes[0]-1) * (args.primes[1]-1)
+        phi = (args.primes[0] - 1) * (args.primes[1] - 1)
         print("phi:")
         print(phi)
 
@@ -41,6 +90,7 @@ def main():
         c = pow(m, e, n)
         print("c:")
         print(c)
+
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
+
 setup(
-    name='rsa-keygen',
-    entry_points={
-      'console_scripts': [
-          'rsa-keygen = rsa:main',
-      ],
-    },
-    version='0.0.1',
-    description='Generate large textbook integer-type RSA schema',
+    name="rsa-keygen",
+    entry_points={"console_scripts": ["rsa-keygen = rsa:main",],},
+    version="0.0.1",
+    description="Generate large textbook integer-type RSA schema",
     long_description=readme,
-    author='Alan Yee',
-    author_email='alanyee@users.noreply.github.com',
-    url='https://github.com/alanyee/rsa-keygen',
-    packages=find_packages(exclude=('tests', 'docs')),
+    author="Alan Yee",
+    author_email="alanyee@users.noreply.github.com",
+    url="https://github.com/alanyee/rsa-keygen",
+    packages=find_packages(
+        where="rsactf", include=("Cryptodome"), exclude=("tests", "docs")
+    ),
     include_package_data=True,
-    python_requires='>=3.8',
+    python_requires=">=3.8",
     extras_require={
-        'testing': ["pylint", "pytest", "mypy"]
-    }
+        "normal": ["pycryptodomex"],
+        "testing": ["pylint", "pytest", "mypy"],
+    },
 )
